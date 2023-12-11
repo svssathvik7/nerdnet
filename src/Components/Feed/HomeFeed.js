1cbe81a649710051197d5a0ed5a7b60e3bad91e9
SATHVIK SVS <svssathvik77@gmail.com>
2023-12-11 09:36:51 +0530

    dynamically getting data from backend

--- src/Components/Feed/HomeFeed.js
+++ src/Components/Feed/HomeFeed.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Post from '../Post/Post'
-import postConstants from '../../constants/postConstants'
 import "./HomeFeed.css";
+import { useState } from 'react';
+import axios from 'axios';
 export default function HomeFeed() {
+  const [posts,setPosts] = useState([]);
+  useEffect(
+    ()=>{
+      const getPosts = async ()=>{
+        const allPosts = (await axios.get("http://localhost:3500/api/posts/getAllPosts")).data;
+        setPosts(allPosts);
+      }
+      getPosts();
+    }
+  ,[]);
   return (
     <div id='home-feed' className='flex flex-col items-center justify-center'>
-        <div id='home-feed-scroller' className='flex flex-col items-center justify-start'>
-            {postConstants.map(post => (
+        {posts.length > 0 ? <div id='home-feed-scroller' className='flex flex-col items-center justify-start'>
+            {posts.map(post => (
                 <div className='single-feed m-2' key={post.id}>
                     <Post {...post}/>
                 </div>
             ))}
-        </div>
+        </div> : <h1 className='text-5xl font-bold'>Feed got covered!</h1>}
     </div>
   )
 }
