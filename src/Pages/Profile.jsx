89b18d9a4bb0cd0f98517ae93826393794b8f1fd
SATHVIK SVS <svssathvik77@gmail.com>
2023-12-12 22:03:59 +0530

    responsive profile page completed

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,11 +3,12 @@ import Header from '../Partials/Header'
 import AddPostBtn from '../Components/AddPost/AddPostBtn'
 import ProfileSidebar from '../Components/ProfileComponents/ProfileSidebar'
 import UserPosts from '../Components/ProfileComponents/UserPosts'
-export default function Profile() {
+import { connect } from 'react-redux'
+function Profile({data}) {
   return (
     <div id='profile'>
       <Header/>
-      <div id="profile-container" className='flex items-center justify-start'>
+      <div id="profile-container" className={`flex ${data.isMobile ? " flex-col " : " "} items-center justify-start`}>
         <ProfileSidebar/>
         <UserPosts/>
       </div>
@@ -15,3 +16,12 @@ export default function Profile() {
     </div>
   )
 }
+const mapStateToProps = (state)=>{
+  return {
+    data : {
+      isMobile : state.isMobile,
+    }
+  }
+}
+export default connect(mapStateToProps)(Profile);
+
