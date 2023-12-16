27089b17676f52fe82675743b6ddfdfcb774b25a
SATHVIK SVS <svssathvik77@gmail.com>
2023-12-16 09:08:08 +0530

    preloader added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ import Profile from './Pages/Profile';
 import AboutLanding from './DescriptorPage/AboutLanding';
 import Home from './Pages/Home';
 import AddPostForm from './Components/AddPost/AddPostForm';
+import { useState,useEffect } from 'react';
+import Loader from "./assets/Loader.json";
+import Lottie from 'lottie-react';
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(
+    ()=>{
+      setLoading(true);
+
+      const timeoutId = setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+      return () => clearTimeout(timeoutId);
+    }
+  ,[]);
   return (
+    loading ?
+      <div className='absolute w-screen h-screen top-0 bottom-0 left-0 right-0 m-auto flex items-center justify-center bg-white'>
+        <Lottie loop={true} animationData={Loader} className='w-52'/>
+      </div>
+    :
     <div className="App">
       <Routes>
         <Route path='/' element={<Landing/>}/>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,28 +1,31 @@
-import React, { useEffect } from 'react'
-import Header from '../Partials/Header'
-import MiniNavBar from '../Components/Navbar/MiniNavBar'
-import { connect } from 'react-redux'
-import HomeFeed from '../Components/Feed/HomeFeed'
-import AddPostBtn from '../Components/AddPost/AddPostBtn'
-import AsideBar from '../Partials/AsideBar'
-function Home({data}) {
+import React from 'react';
+import { connect } from 'react-redux';
+import Header from '../Partials/Header';
+import MiniNavBar from '../Components/Navbar/MiniNavBar';
+import HomeFeed from '../Components/Feed/HomeFeed';
+import AddPostBtn from '../Components/AddPost/AddPostBtn';
+import AsideBar from '../Partials/AsideBar';
+
+function Home({ data }) {
   return (
     <div id='home-page' className=''>
-          <Header/>
-          {data.isMobile ? <MiniNavBar/> : <></>}
-          <div className={`flex items-center ${data.isMobile ? " justify-center " : " justify-start "}`}>
-            {data.isMobile ? <></> : <AsideBar/>}
-            <HomeFeed/>
-          </div>
-          {data.isMobile ? <></> : <AddPostBtn/>}
+      <Header />
+      {data.isMobile ? <MiniNavBar /> : null}
+      <div className={`flex items-center ${data.isMobile ? " justify-center " : " justify-start "}`}>
+        {data.isMobile ? null : <AsideBar />}
+        <HomeFeed />
       </div>
-  )
+      {data.isMobile ? null : <AddPostBtn />}
+    </div>
+  );
 }
-const mapStateToProps = (state)=>{
+
+const mapStateToProps = (state) => {
   return {
-    data : {
-      isMobile : state.isMobile,
+    data: {
+      isMobile: state.isMobile,
     }
-  }
-}
+  };
+};
+
 export default connect(mapStateToProps)(Home);
