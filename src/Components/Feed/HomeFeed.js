6a9b0b64a5846bbcbc91ab9799962225a64f9d92
SATHVIK SVS <svssathvik77@gmail.com>
2023-12-28 16:49:29 +0530

    styled home feed covered

--- src/Components/Feed/HomeFeed.js
+++ src/Components/Feed/HomeFeed.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import Post from '../Post/Post'
 import "./HomeFeed.css";
 import { useState } from 'react';
+import Lottie from 'lottie-react';
+import DoneAnimation from "../../assets/doneAnimation.json";
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import {homeFeedContextProvider} from "../../Context/homeFeedContext";
@@ -22,14 +24,19 @@ export default function HomeFeed() {
     }
   ,[posts,location.pathname]);
   return (
-    <div id='home-feed' className='flex flex-col items-center justify-center'>
+    <div id='home-feed' className='flex flex-col items-center justify-center text-center'>
         {homeFeed.length > 0 ? <div id='home-feed-scroller' className='flex flex-col items-center justify-start'>
             {homeFeed.map((post,i) => (
                 <div className='single-feed m-2' key={i}>
                     <Post {...post}/>
                 </div>
             ))}
-        </div> : <h1 className='text-5xl font-bold'>Feed got covered!</h1>}
+        </div> : 
+        <div id='feed-covered-message' className='flex items-center justify-center text-center'>
+          <h1 className='text-5xl font-bold'>Feed got covered!</h1>
+          <Lottie animationData={DoneAnimation} className='w-64 h-fit p-0' loop={true}/>
+        </div>
+        }
     </div>
   )
 }
