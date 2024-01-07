c470c7f2de011ab9a2e212373852f3242880265e
SATHVIK SVS <svssathvik77@gmail.com>
2024-01-07 22:49:18 +0530

    safest version 2.0

--- src/Partials/AsideBar.jsx
+++ src/Partials/AsideBar.jsx
@@ -3,6 +3,7 @@ import "./AsideBar.css";
 import { Link } from 'react-router-dom';
 import TopNerdIcon from "../assets/bright-idea.svg";
 import TopTopicIcon from "../assets/test-tubes.svg";
+import TopPostIcon from "../assets/article.svg";
 import { statContextProvider } from '../Context/statContext';
 function TrendingNerdsDiv(){
   const {trendingNerds} = useContext(statContextProvider);
@@ -40,6 +41,24 @@ function TrendingTopicsDiv(){
     </div>
   )
 }
+function TrendingPostsDiv(){
+  const {trendingTopics} = useContext(statContextProvider);
+  return (
+    <div className='flex flex-wrap flex-col my-4'>
+    <div className='flex items-center justify-start'>
+      <img title='TrendingNerds' className='w-8' alt='emoji' src={TopPostIcon}/>
+      <h6 className='font-medium underline text-slate-200'>Trending Posts</h6>
+    </div>
+    <ol className='list-decimal pl-4 flex flex-col items-center justify-start text-white'>
+      {trendingTopics.map((value, i) => (
+          <li key={i}>
+            <p>{value._id}<sub>({value.count})</sub></p>
+          </li>
+      ))}
+    </ol>
+    </div>
+  )
+}
 function RedirectFooter(){
   return (
     <div id='semi-footer' className='flex w-[100%] items-center justify-around mt-4'>
@@ -61,6 +80,7 @@ export default function AsideBar() {
     <div className='flex flex-col items-center justify-start p-2' id='aside-bar'>
         <TrendingNerdsDiv/>
         <TrendingTopicsDiv/>
+        <TrendingPostsDiv/>
         <RedirectFooter/>
     </div>
   )
