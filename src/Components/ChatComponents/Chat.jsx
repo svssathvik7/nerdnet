03ca2ed0917f5ba37725dcd53e76b97c217429a8
SATHVIK SVS <svssathvik77@gmail.com>
2024-01-27 23:03:31 +0530

    chatting app design features added with perfect styling 3.2.0

--- src/Components/ChatComponents/Chat.jsx
+++ src/Components/ChatComponents/Chat.jsx
@@ -2,12 +2,15 @@ import React, { useContext, useEffect, useState } from 'react'
 import { userContextProvider } from '../../Context/userContext';
 import "./Chat.css";
 import axios from 'axios';
+import { IoClose } from "react-icons/io5";
+import { SyncLoader } from 'react-spinners';
 export default function Chat() {
     const [active,setActive] = useState(0);
     const {user} = useContext(userContextProvider);
     const [client,setClient] = useState(user);
     const [value,setValue] = useState('');
     const [messages,setMessages] = useState([]);
+    const [loading,setLoading] = useState(false);
     const fetchChat = async ()=>{
         try{
             const users = [user._id,client._id].sort();
@@ -30,6 +33,7 @@ export default function Chat() {
     const handleSendMessage = async ()=>{
         const users = [user._id,client._id].sort();
         try{
+            setLoading(true);
             const chatId = users[0]+users[1];
             const response = (await axios.post(process.env.REACT_APP_BACKEND_URL+"/chat/add-message/",{
                 chatId : chatId,
@@ -48,6 +52,7 @@ export default function Chat() {
         catch(error){
             console.log(error);
         }
+        setLoading(false);
     }
     useEffect(
         ()=>{
@@ -56,26 +61,24 @@ export default function Chat() {
     ,[client,user,messages]);
   return (
     <div className='absolute bottom-2 left-2 m-2 cursor-pointer w-fit flex items-center justify-start'>
-      <div className='bg-white w-12 h-12 rounded-full flex items-center justify-center self-end' onClick={()=>{setActive(!active)}}>
-        {active ? "X" : "O"}
+      <div className='bg-white w-12 h-12 rounded-full flex items-center justify-center' onClick={()=>{setActive(!active)}}>
+        {active ? <p>X</p> : <p>O</p>}
       </div>
-      {active==1 && <div id='online-frnds-container'>
+      {active==1 && <div id='online-frnds-container' className='flex items-center justify-start'>
         {user && user.following && user.following.map((member,i)=>(
-            <div key={i}>
+            <div key={i} className='flex'>
                 <img alt='dp' src={member.dp} className='w-12 h-12 rounded-full mx-1 bg-white p-1' onClick={()=>{setClient(member);setActive(2)}}/>
             </div>
         ))}
       </div>}
       {active==2 && 
-        <div id='chat-box' className='bg-white m-2 border-2 border-black rounded-lg p-1 flex flex-col items-center justify-start'>
+        <div id='chat-box' className='bg-white m-2 border-2 border-black rounded-lg p-1 flex flex-col items-center justify-start trans300'>
             <div id='chat-header' className='h-1/6 bg-green-500 w-full flex items-center justify-between p-1'>
                 <div className='flex items-center justify-start mx-1'>
                     <img alt='dp' src={client.dp} className='w-12 h-12 rounded-full bg-black p-1'/>
                     <p className='font-bold text-xl ml-1'>{client.username}</p>
                 </div>
-                <div className='text-red-600 font-extrabold mx-1' onClick={()=>{setActive(0)}}>
-                    X
-                </div>
+                <IoClose color='red' className='font-extrabold mx-1 text-2xl' onClick={()=>{setActive(0)}}/>
             </div>
             <div id='chat-scroller'>
                 {
@@ -83,21 +86,33 @@ export default function Chat() {
                         <div key={i} className='flex w-full flex-col'>
                             {
                                 (message?.user == user?._id)?
-                                <div className='p-2 self-end bg-red-500 message m-1'>
-                                    <p>{message.message}</p>
+                                <div className='p-2 self-end bg-black text-white message m-1 flex-wrap flex flex-col'>
+                                    <div className='flex items-center justify-start msg-box flex-wrap p-1'>
+                                        <p className='flex-wrap'>{message.message}</p>
+                                        <img alt='dp' src={user?.dp} className='w-6 h-6 rounded-full mx-1'/>
+                                    </div>
+                                    <div className='text-xs'>
+                                        <p>{message.timestamp}</p>
+                                    </div>
                                 </div>
                                 :
-                                <div className='p-2 self-start bg-blue-600 message m-1'>
-                                    <p>{message.message}</p>
+                                <div className='p-2 self-start bg-slate-600 text-white message m-1 flex-wrap flex flex-col'>
+                                    <div className='flex items-center justify-start msg-box flex-wrap p-1'>
+                                        <img alt='dp' src={client?.dp} className='w-6 h-6 rounded-full mx-1'/>
+                                        <p className='flex-wrap'>{message.message}</p>
+                                    </div>
+                                    <div className='text-xs'>
+                                        <p>{message.timestamp}</p>
+                                    </div>
                                 </div>
                             }
                         </div>
                     ))
                 }
             </div>
-            <div id='chat-controller' className='m-2'>
+            <div id='chat-controller' className='m-2 flex items-center justify-start'>
                 <input type='text' className='border-b-2 border-black' name='chatInput' placeholder='Enter message...' onChange={(e)=>{setValue(e.target.value);}} value={value}/>
-                <button onClick={handleSendMessage} className='mx-1 p-1 bg-black text-white font-normal rounded-lg'>Send</button>
+                {loading ? <div className='bg-black w-fit mx-1 p-2 rounded-lg flex items-center justify-center h-fit'><SyncLoader color="#fff" size={15}/></div> : <button onClick={handleSendMessage} className='mx-1 p-1 bg-black text-white font-normal rounded-lg'>Send</button>}
             </div>
         </div>
       }
