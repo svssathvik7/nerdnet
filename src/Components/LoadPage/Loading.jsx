06f3f2f9370a5cd350d9856408a6ba86e63f3377
SATHVIK SVS <svssathvik77@gmail.com>
2024-02-17 23:41:12 +0530

    cleared a styling bug in explore

--- src/Components/LoadPage/Loading.jsx
+++ src/Components/LoadPage/Loading.jsx
@@ -3,7 +3,7 @@ import LoaderAnimation from "../../assets/LoaderAnimation.json";
 import Lottie from 'lottie-react';
 export default function Loading() {
   return (
-    <div className='w-screen h-screen bg-black absolute top-0 left-0 z-50 flex items-center justify-center'>
+    <div className='w-screen h-screen bg-black absolute top-0 left-0 z-50 flex items-center justify-center overflow-hidden'>
         <div className='flex items-center justify-center'>
           <Lottie animationData={LoaderAnimation}/>
         </div>
