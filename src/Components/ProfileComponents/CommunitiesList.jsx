1d23958e186a4cf31001b405f0087c76d05edc5d
SATHVIK SVS <svssathvik77@gmail.com>
2024-03-11 20:34:50 +0530

    space list on profile showing

--- src/Components/ProfileComponents/CommunitiesList.jsx
+++ src/Components/ProfileComponents/CommunitiesList.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react'
 import "./CommunitiesList.css";
 import { statContextProvider } from '../../Context/statContext';
+import { Link } from 'react-router-dom';
 export default function CommunitiesList() {
     const {spaces} = useContext(statContextProvider);
   return (
     <div className='flex items-center justify-evenly flex-wrap communities-list'>
       {spaces && spaces.map((space,i)=>(
-        <div key={i} className='bg-white p-1 rounded-lg opacity-80 hover:opacity-100 space-card'>
+        <Link to={"/community/"+space._id} key={i} className='bg-white p-1 rounded-lg opacity-80 hover:opacity-100 space-card'>
             <div className='flex flex-col items-end justify-end flex-wrap'>
                 <img alt='cover' src={space.coverPic} className='w-full aspect-video object-contain'/>
                 <div className='p-1 bg-white rounded-lg absolute m-2'>
@@ -17,7 +18,7 @@ export default function CommunitiesList() {
                 <h4 className='font-bold text-3xl w-fit'>{space?.name}</h4>
                 <p className='mx-1 w-fit text-sm rounded-lg bg-yellow-400 text-black'>#{space?.subject}</p>
             </div>
-        </div>
+        </Link>
       ))}
     </div>
   )
